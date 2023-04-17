b3b145312fc8e0afa28c04cedfb57a2bd349c688
Draper <27962761+Drapersniper@users.noreply.github.com>
2023-04-17 19:33:59 +0100

    update

--- custom-build.sh
+++ custom-build.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env bash
 
-/home/draper/RiderProjects/Sonarr/build.sh -r "linux-musl-x64"  -f "net6.0" --all --docker
+/home/draper/RiderProjects/Sonarr/build.sh -r "linux-musl-x64"  -f "net6.0" --docker --all
